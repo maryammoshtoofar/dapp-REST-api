28ee3097d4218dcee40f30fa19be73f5c2009b81
Maryam Moshootfar <maryammoshtoofar@gmail.com>
2023-06-11 19:01:33 +0330

    add binary bonus api

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ require("dotenv").config();
 const API_URL = process.env.API_URL;
 const ERC20_ADMIN_WALLET = process.env.ERC20_ADMIN_WALLET;
 const DAPP_TOKEN_CONTRACT = process.env.DAPP_TOKEN_CONTRACT;
-const abi = require("./ABI/tokenAbi.json");
+const GENEALOGY_CONTRACT = process.env.GENEALOGY_CONTRACT;
+const ABI = {
+  dappToken: require("./ABI/tokenAbi.json"),
+  genealogy: require("./ABI/genealogyAbi.json"),
+  tokenFarm: require("./ABI/tokenFarm.json"),
+};
 const web3 = new Web3(API_URL);
 
 const wallet = web3.eth.accounts.privateKeyToAccount("0x" + ERC20_ADMIN_WALLET);
-const contractInstance = new web3.eth.Contract(abi, DAPP_TOKEN_CONTRACT);
 
+const dappTokenContract = new web3.eth.Contract(
+  ABI.dappToken,
+  DAPP_TOKEN_CONTRACT
+);
+const genealogyContract = new web3.eth.Contract(
+  ABI.genealogy,
+  GENEALOGY_CONTRACT
+);
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -23,6 +35,25 @@ app.post("/transfer", async (req, res) => {
   }
 });
 
+// Genealogy Bonuses
+
+app.post("/binarybonus", async (req, res) => {
+  try {
+    const gas = await genealogyContract.methods
+      .binaryBonus()
+      .estimateGas({ from: wallet.address });
+
+    await genealogyContract.methods
+      .binaryBonus()
+      .send({ from: wallet.address, gas: gas })
+      .then((res) => {
+        result = res;
+      });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`app listening at http://localhost:${port}`);
@@ -32,11 +63,11 @@ const transferToken = async (amount, receiver) => {
   let result;
   web3.eth.accounts.wallet.add(ERC20_ADMIN_WALLET);
   const value = web3.utils.toWei(amount);
-  const gas = await contractInstance.methods
+  const gas = await dappTokenContract.methods
     .transfer(receiver, value)
     .estimateGas({ from: wallet.address });
 
-  await contractInstance.methods
+  await dappTokenContract.methods
     .transfer(receiver, value)
     .send({ from: wallet.address, gas: gas })
     .then((res) => {
